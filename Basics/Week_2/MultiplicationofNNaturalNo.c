1663a10a3fbb541b9c2696b7034cd71af755cb42
AmmarM2005 <itsammarm52@gmail.com>
2026-08-02 01:05:42 +0530

    C: Made Multiplication of N Natural Numbers

---
+++ Basics/Week_2/MultiplicationofNNaturalNo.c
@@ -0,0 +1,14 @@
+//Program of Print Multiplication of N Natural Numbers
+#include<stdio.h>
+int main()
+{
+    int n,i,mul=1;
+    printf("Enter the Value of N:");
+    scanf("%d",&n);
+    for(i=1; i<=n;i++)
+    {
+        mul=mul*i;
+    }
+    printf("The Multiplication of N Natural Numbers is:%d",mul);
+    return 0;
+}
